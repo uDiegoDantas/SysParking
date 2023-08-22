de4e4847e6907a5b6754936f2f2b452420d17d7b
Diego Dantas <diegodantasjsrn@gmail.com>
2023-08-21 21:37:48 -0300

    Add files via upload

---
+++ sp.c
@@ -0,0 +1,158 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/////
+// Assinatura das funções
+void tela_menu_principal(void);
+void tela_menu_veiculo(void);
+void tela_cadastrar_veiculo(void);
+void tela_pesquisar_veiculo(void);
+void tela_alterar_veiculo(void);
+void tela_excluir_veiculo(void);
+
+/////
+// Programa principal
+int main(void) {
+    tela_menu_principal();
+    tela_menu_veiculo();
+    tela_cadastrar_veiculo();
+    tela_pesquisar_veiculo();
+    tela_alterar_veiculo();
+    tela_excluir_veiculo();
+    return 0;
+}
+
+
+/////
+// Funções
+
+void tela_menu_principal(void) {
+    char op;
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = =         SysParking        = = = = =                ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            1. Carros                                                    ///\n");
+    printf("///            0. Sair                                                      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Escolha a opcao desejada: ");
+    scanf("%c", &op);
+    getchar();
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t>>> Pressione <ENTER> para continuar...\n");
+    getchar();
+}
+
+
+void tela_menu_veiculo(void) {
+    char op;
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
+    printf("///            = = = = = = = = = Menu Veiculos = = = = = = = =              ///\n");
+    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            1. Cadastrar um novo veiculo                                 ///\n");
+    printf("///            2. Pesquisar os dados de um veiculo                          ///\n");
+    printf("///            3. Atualizar o cadastro de um veiculo                        ///\n");
+    printf("///            4. Excluir um veiculo do sistema                             ///\n");
+    printf("///            0. Voltar ao menu anterior                                   ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Escolha a opção desejada: ");
+    scanf("%c", &op);
+    getchar();
+    printf("///                                                                         ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t>>> Pressione <ENTER> para continuar...\n");
+    getchar();
+}
+
+
+void tela_cadastrar_veiculo(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
+    printf("///            = = = = = = = = Cadastrar veiculo = = = = = = =              ///\n");
+    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Placa:                                                       ///\n");
+    printf("///            Nome do proprietário:                                        ///\n");
+    printf("///            Nome do veiculo:                                             ///\n");
+    printf("///            Cor do veiculo:                                              ///\n");
+    printf("///            Ano do veiculo (dd/mm/aaaa):                                 ///\n");
+    printf("///            Celular do proprietário (apenas números):                    ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+
+void tela_pesquisar_veiculo(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
+    printf("///            = = = = = = = Pesquisar Veiculo = = = = = = = =              ///\n");
+    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Informe a placa do veiculo:                                  ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t>>> Pressione <ENTER> para continuar...\n");
+    getchar();
+}
+
+
+void tela_alterar_veiculo(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
+    printf("///            = = = = = = = = Alterar Veiculo = = = = = = = =              ///\n");
+    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Informe a placa do veiculo:                                  ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t>>> Pressione <ENTER> para continuar...\n");
+    getchar();
+}
+
+
+void tela_excluir_veiculo(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
+    printf("///            = = = = = = = = Excluir Veiculo = = = = = = = =              ///\n");
+    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Informe a placa do veiculo que deseja excluir:               ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t>>> Pressione <ENTER> para continuar...\n");
+    getchar();
+}
