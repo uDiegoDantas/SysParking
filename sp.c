93ae0f4a01241194e71e85a2951125ac790a365c
Diego Dantas <diegodantasjsrn@gmail.com>
2023-09-01 10:04:50 -0300

    Atualização com todas as telas do projeto

--- sp.c
+++ sp.c
@@ -1,32 +1,49 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
-/////
-// Assinatura das funÃ§Ãµes
+// Assinatura das funcoes
 void tela_menu_principal(void);
+// Modulo de veiculos
 void tela_menu_veiculo(void);
 void tela_cadastrar_veiculo(void);
 void tela_pesquisar_veiculo(void);
 void tela_alterar_veiculo(void);
 void tela_excluir_veiculo(void);
+void visualizar_veiculo(void);
+// Modulo de clientes
+void tela_menu_cliente(void);
+void tela_cadastrar_cliente(void);
+void tela_editar_cliente(void);
+void tela_excluir_cliente(void);
+void visualizar_cliente(void);
+// Modulo de vagas do estacionamento
+void tela_menu_vagas(void);
+void tela_criar_vaga(void);
+void tela_editar_vaga(void);
+void tela_excluir_vaga(void);
 
-/////
 // Programa principal
 int main(void) {
     tela_menu_principal();
-    tela_menu_veiculo();
-    tela_cadastrar_veiculo();
-    tela_pesquisar_veiculo();
-    tela_alterar_veiculo();
-    tela_excluir_veiculo();
+	tela_menu_veiculo();
+	tela_cadastrar_veiculo();
+	tela_pesquisar_veiculo();
+	tela_alterar_veiculo();
+	tela_excluir_veiculo();
+	visualizar_veiculo();
+	tela_menu_cliente();
+	tela_cadastrar_cliente();
+	tela_editar_cliente();
+	tela_excluir_cliente();
+	visualizar_cliente();
+	tela_menu_vagas();
+	tela_criar_vaga();
+	tela_editar_vaga();
+	tela_excluir_vaga();
     return 0;
 }
 
-
-/////
-// FunÃ§Ãµes
-
+// Funcoes
 void tela_menu_principal(void) {
     char op;
     system("clear||cls");
@@ -36,6 +53,8 @@ void tela_menu_principal(void) {
     printf("///            = = = = =         SysParking        = = = = =                ///\n");
     printf("///                                                                         ///\n");
     printf("///            1. Carros                                                    ///\n");
+    printf("///            2. Clientes                                                  ///\n");
+    printf("///            3. Vagas                                                     ///\n");
     printf("///            0. Sair                                                      ///\n");
     printf("///                                                                         ///\n");
     printf("///            Escolha a opcao desejada: ");
@@ -47,7 +66,6 @@ void tela_menu_principal(void) {
     printf("\t\t>>> Pressione <ENTER> para continuar...\n");
     getchar();
 }
-
 
 void tela_menu_veiculo(void) {
     char op;
@@ -76,7 +94,6 @@ void tela_menu_veiculo(void) {
     getchar();
 }
 
-
 void tela_cadastrar_veiculo(void) {
     system("clear||cls");
     printf("\n");
@@ -87,11 +104,10 @@ void tela_cadastrar_veiculo(void) {
     printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
     printf("///                                                                         ///\n");
     printf("///            Placa:                                                       ///\n");
-    printf("///            Nome do proprietÃ¡rio:                                        ///\n");
-    printf("///            Nome do veiculo:                                             ///\n");
+    printf("///            Codigo do proprietario:                                      ///\n");
+    printf("///            Tipo do veiculo:                                             ///\n");
     printf("///            Cor do veiculo:                                              ///\n");
     printf("///            Ano do veiculo (dd/mm/aaaa):                                 ///\n");
-    printf("///            Celular do proprietÃ¡rio (apenas nÃºmeros):                    ///\n");
     printf("///                                                                         ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
@@ -100,6 +116,20 @@ void tela_cadastrar_veiculo(void) {
     getchar();
 }
 
+void visualizar_veiculo(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = = = = =  Visualizar Veiculo = = = = = = = =        ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Informe a placa do veiculo a ser visualizado:                ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
 
 void tela_pesquisar_veiculo(void) {
     system("clear||cls");
@@ -119,7 +149,6 @@ void tela_pesquisar_veiculo(void) {
     getchar();
 }
 
-
 void tela_alterar_veiculo(void) {
     system("clear||cls");
     printf("\n");
@@ -138,7 +167,6 @@ void tela_alterar_veiculo(void) {
     getchar();
 }
 
-
 void tela_excluir_veiculo(void) {
     system("clear||cls");
     printf("\n");
@@ -154,5 +182,161 @@ void tela_excluir_veiculo(void) {
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
     printf("\t\t>>> Pressione <ENTER> para continuar...\n");
+    getchar();
+}
+
+void tela_menu_cliente(void) {
+    char op;
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = = = = = = Menu Clientes = = = = = = = =            ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            1. Cadastrar um novo cliente                                 ///\n");
+    printf("///            2. Editar dados de um cliente                                ///\n");
+    printf("///            3. Excluir um cliente do sistema                             ///\n");
+    printf("///            4. Visualizar informações de um cliente                      ///\n");
+    printf("///            0. Voltar ao menu anterior                                   ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Escolha a opção desejada: ");
+    scanf("%c", &op);
+    getchar();
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t>>> Pressione <ENTER> para continuar...\n");
+    getchar();
+}
+
+void tela_cadastrar_cliente(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = = = = Cadastrar Cliente = = = = = = =              ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Nome:                                                        ///\n");
+    printf("///            CPF:                                                         ///\n");
+    printf("///            Telefone:                                                    ///\n");
+    printf("///            Endereço:                                                    ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void tela_editar_cliente(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = = = = = = Editar Cliente = = = = = = = =            ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Informe o CPF do cliente a ser editado:                       ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void tela_excluir_cliente(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = = = = = = Excluir Cliente = = = = = = = =          ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Informe o CPF do cliente a ser excluído:                     ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void visualizar_cliente(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = = = = = = Visualizar Cliente = = = = = = = =        ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Informe o CPF do cliente a ser visualizado:                   ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void tela_menu_vagas(void) {
+    char op;
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = = = = = = Menu Vagas = = = = = = = =              ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            1. Criar uma nova vaga                                        ///\n");
+    printf("///            2. Editar vaga existente                                      ///\n");
+    printf("///            3. Excluir vaga                                               ///\n");
+    printf("///            0. Voltar ao menu anterior                                   ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Escolha a opção desejada: ");
+    scanf("%c", &op);
+    getchar();
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t>>> Pressione <ENTER> para continuar...\n");
+    getchar();
+}
+
+void tela_criar_vaga(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = = = = = Criar Vaga = = = = = = = =                ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Informe o número da vaga:                                    ///\n");
+    printf("///            Tipo da vaga (carro, moto, etc.):                            ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void tela_editar_vaga(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = = = = = Editar Vaga = = = = = = = =              ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Informe o número da vaga a ser editada:                       ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void tela_excluir_vaga(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = = = = = Excluir Vaga = = = = = = = =            ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Informe o número da vaga a ser excluída:                     ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
 }
