73f0d19f8e19f70bf3c462fde59e38166a0ba023
Diego Dantas <diegodantasjsrn@gmail.com>
2023-12-14 00:06:55 -0300

    Update telas_servicos.h

--- telas_servicos.h
+++ telas_servicos.h
@@ -3,3 +3,5 @@ void tela_menu_servicos(void);
 void tela_checkin(void);
 void listar_checkins(void);
 void tela_checkout(void);
+int existeCheckin(const char *placa, const char *codigo, const char *cpf);
+void editar_checkin(void);
